ded336fe81b875bb5a6897440db6d4bf61a64855
Shahrukh Haider <shahrukh.haider35@gmail.com>
2021-05-28 16:25:19 -0500

    skipping turns when no move available

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { drawStartRects } from './components/Start';
 import { drawToken } from './components/Tokens';
 import Game from './actors/game';
 import { GAME_STATUS } from './constants';
-import { getGameStatusLabel, rollDice, startGame } from './utils/game';
+import { getGameStatusLabel, rollDice, startGame, changeTurns } from './utils/game';
 
 function App() {
   const [cells, updateCells] = useState(createCells());
@@ -42,6 +42,11 @@ function App() {
         disabled={game.status !== GAME_STATUS.finished && game.status !== GAME_STATUS.not_started}
         onClick={() => startGame(game, updateGame)}>
         Start Game
+      </button>
+      <button
+        disabled={!(game.status === GAME_STATUS.waiting_for_token)}
+        onClick={() => changeTurns(game, updateGame)}>
+        Skip Turn
       </button>
     </div>
   );
--- src/utils/game.js
+++ src/utils/game.js
@@ -142,5 +142,6 @@ export {
     startGame,
     getGameStatusLabel,
     rollDice,
-    updateTokenPostion
+    updateTokenPostion,
+    changeTurns
 }
